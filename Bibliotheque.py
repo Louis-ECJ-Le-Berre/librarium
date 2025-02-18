9d0d2eb1f5772abe8b7ed1b6f75687e3ab64a449
Louis-ECJ-Le-Berre <louis.e.c.leberre@gmail.com>
2025-02-18 16:53:51 +0100

    Base de bibliohtèque

---
+++ Bibliotheque.py
@@ -0,0 +1,25 @@
+class Bibliotheque :
+
+    def __init__(self) :
+        # Doit chercher dans le tableur excel toutes les informations pour les regrouper dans ses attributs
+        pass
+
+    def ajout(self, document) :
+        # Prend un objet document et l'ajoute dans la bibliothèque avec toutes ses informations
+        pass
+
+    def mise_a_jour(self) :
+        # Prend les informations de l'objet et les traduit en un nouveau document excel
+        pass
+
+    def write(self, square, string) :
+        # Ecris le contenu de string dans la square
+        pass
+
+    def read(self, square) :
+        # Renvoie ce qui est contenu dans la square
+        pass
+
+    def liste_mc(self, mc):
+        # Print la liste de tous les objets qui ont le mot-clef mc
+        pass
