189cdeaf5f2c750f0d6ec4a918e1337d39123922
Louis-ECJ-Le-Berre <louis.e.c.leberre@gmail.com>
2025-02-19 11:58:18 +0100

    Lecture de la BDD et création de la liste de documents

--- Bibliotheque.py
+++ Bibliotheque.py
@@ -6,28 +6,27 @@ from odf.text import P
 
 from utilities import creer_ligne
 
+from Document import Document
+
 class Bibliotheque :
 
-    def __init__(self, nom_doc) :
+    def __init__(self, nom_BDD) :
         # Doit chercher dans le tableur excel toutes les informations pour les regrouper dans ses attributs
-        self.doc = load("BDD_Docs_Admin.ods")
-        self.table = self.doc.spreadsheet.getElementsByType(Table)[0]
-        pass
+        self.bdd = load(nom_BDD)
+        self.table = self.bdd.spreadsheet.getElementsByType(Table)[0]
+        self.all_docs = self.get_all_documents(self.table)
+
 
     def ajout(self, document) :
-        # Prend un objet document et l'ajoute dans la bibliothèque avec toutes ses informations
+        """Prend un objet document et l'ajoute dans la bibliothèque avec toutes ses informations. 
+        Renvoie un objet bibliohtèque mis à jour selon la nouvelle base de données"""
 
         liste_attributs = document.infos #On récupère les informations de l'objet document
         ligne = creer_ligne(liste_attributs)
         self.table.addElement(ligne)
-        self.doc.save("BDD_Docs_Admin.ods")
-        self.mise_a_jour()
-
-    def mise_a_jour(self) :
-        # Met à jour les informations de l'objet pour correspondre à la base de données
-
-        self.doc = load("BDD_Docs_Admin.ods")
-        self.table = self.doc.spreadsheet.getElementsByType(Table)[0]
+        self.bdd.save("BDD_Docs_Admin.ods")
+        
+        return Bibliotheque("BDD_Docs_Admin.ods")
 
     def write(self, square, string) :
         # Ecris le contenu de string dans la square
@@ -40,3 +39,30 @@ class Bibliotheque :
     def liste_mc(self, mc):
         # Print la liste de tous les objets qui ont le mot-clef mc
         pass
+
+    def get_all_documents(self, table):
+        """Prend une table de type odfpy et renvoie une liste contenant tous les documents de cette table"""
+
+        liste_documents = []
+        rows = table.getElementsByType(TableRow)
+
+        for ligne in rows :
+            new_doc = self.create_document_from_row(ligne)
+            liste_documents.append(new_doc)
+
+        return liste_documents
+
+
+    def create_document_from_row(self, odfpy_row):
+        """Prend une ligne de type odfpy, en lit les cellules et renvoie un objet document"""
+
+        liste = []
+
+        for cellule in odfpy_row.getElementsByType(TableCell):
+            text_elements = cellule.getElementsByType(P)
+            if text_elements:
+                liste.append(text_elements[0].firstChild.data)  # Ajouter le texte
+            else:
+                liste.append("")
+
+        return Document(liste[0],liste[1],liste[2],liste[3],liste[4])
--- Document.py
+++ Document.py
@@ -1,22 +1,21 @@
 class Document :
 
-    def __init__(self, nature, categorie, annee, mois, mots_clefs=[]):
+    def __init__(self, nature, categorie, annee, mois, mots_clefs):
         # Regroupe toutes les informations contenues dans un document
         self.nature = nature
         self.categorie = categorie
         self.annee = annee
         self.mois = mois
-        self.mc = mots_clefs
+        self.mc = self.nettoyer_mc(mots_clefs)
         self.infos = [nature, categorie, annee, mois, mots_clefs]
 
-    def __init__(self, liste_infos):
+    def __str__(self):
+        string = "Ce Document est un(e) " + self.nature + " dans la catégorie " + self.categorie + " datant de " + str(self.mois) + "/" + str(self.annee) + " et a pour mots-clefs : "
+        for mot in self.mc :
+            string += mot
+            string += " ; "
 
-        self.infos = liste_infos
-        self.nature = liste_infos[0]
-        self.categorie = liste_infos[1]
-        self.annee = liste_infos[2]
-        self.mois = liste_infos[3]
-        self.mc = liste_infos[4]
+        return string[:-2]
 
     def modify(self):
         #Modifie un des attributs du document
@@ -29,3 +28,10 @@ class Document :
     def creer_path(self):
         # Créer le path où on peut trouver ce document dans la bibli
         pass
+
+    def nettoyer_mc(self, string):
+        """Prend une string de mots-clefs lue depuis la table avec espace et ; et la transforme en une vraie liste de mots-clefs"""
+
+        new_string = string.replace(" ","")
+        liste = new_string.split(";")
+        return liste
